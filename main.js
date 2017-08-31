336770dfb37b8b521a88ecd771a06d7e535a1674
renzhendong <rzdong163@163.com>
2017-08-31 14:42:35 +0800

    初始代码

---
+++ main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron')
+let win
+function createWindow(){
+  win = new BrowserWindow({width:1000, height:750,frame:false})
+  win.loadURL(`file://${__dirname}/index.html`)
+  // win.webContents.openDevTools()//开启调试工具
+  win.show()
+  win.on('close', () => {
+      win = null
+  })
+  win.on('resize', () => {
+      win.reload()
+  })
+}
+app.on('ready', createWindow)
+app.on('window-all-cloased', () => {
+  if(process.platform !== 'drawin' ){
+      app.quit()
+  }
+})
+app.on('activate', () => {
+  if(win === null){
+      createWindow()
+  }
+})
