a4683dd329d207c6a059a10eac91c8eb9f8de0d2
renzhendong <rzdong163@163.com>
2017-09-07 19:48:42 +0800

    更改启动文件

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const {app, BrowserWindow} = require('electron')
 let win
 function createWindow(){
-  win = new BrowserWindow({width:1000, height:750,frame:false})
+  win = new BrowserWindow({width:1150, height:850,frame:false})
   win.loadURL(`file://${__dirname}/index.html`)
-  win.webContents.openDevTools()//开启调试工具
+  // win.webContents.openDevTools()//开启调试工具
   win.show()
   win.on('close', () => {
       win = null
