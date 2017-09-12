03551d5d3db79b720cff3a1740d9d9511351afe5
renzhendong <renzhendong@qq.com>
2017-09-12 21:36:35 +0800

    评论样式

--- components/ctx_right/right_components/fm/Fm.js
+++ components/ctx_right/right_components/fm/Fm.js
@@ -25,16 +25,16 @@ class Fm extends Component {
     render(){
         // var arr = [0,1,2,3,4,5,6,7,8]
         var arr = [
-            {source: A, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: A, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
             {source: B, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
             {source: C, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
-            {source: D, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: D, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
             {source: E, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
             {source: F, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
-            {source: G, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: G, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
             {source: H, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
             {source: I, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
-            {source: J, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456}
+            {source: J, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456}
         ]
         return (
             <div className="Fm_warper">
