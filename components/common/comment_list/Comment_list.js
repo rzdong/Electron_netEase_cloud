d1bdb34a79c7fec047546bdd612ff417cb73360d
renzhendong <rzdong163@163.com>
2017-09-12 15:03:31 +0800

    增加评论列表

--- components/common/comment_list/Comment_list.js
+++ components/common/comment_list/Comment_list.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react'
 import './Comment_list.css'
 class Comment_list extends Component {
     render(){
-        var index = this.props.item
+        var source = this.props.source
+        var name = this.props.name
+        var content = this.props.content
+        var time = this.props.time
+        var goodNum = this.props.goodNum
         return(
-        <div className="Comment_list">评论内容列表单元{ index }</div>
+        <div className="Comment_list">
+            <div className="Comment_avatar">
+                <div>
+                    <img src={ source } alt=""/>
+                </div>
+            </div>
+            <div className="Comment_ctx">
+                <div className="Comment_content">
+                    <span className="User_name">{ name +": " }</span>
+                    <span>{ content }</span>
+                </div>
+                <div className="Comment_time">
+                    <span>{ time }</span>
+                </div>
+            </div>
+        </div>
         )
     }
 }
--- components/ctx_right/right_components/fm/Fm.js
+++ components/ctx_right/right_components/fm/Fm.js
@@ -10,9 +10,32 @@ import Title_hr from '../../../common/title_hr/Title_hr'
 import Comment from '../../../common/comment/Comment'
 import Comment_list from '../../../common/comment_list/Comment_list'
 
+import A from '../../../../image/img1.jpg'
+import B from '../../../../image/img2.jpg'
+import C from '../../../../image/img3.jpg'
+import D from '../../../../image/img4.jpg'
+import E from '../../../../image/img5.jpg'
+import F from '../../../../image/img6.jpg'
+import G from '../../../../image/img7.jpg'
+import H from '../../../../image/img8.jpg'
+import I from '../../../../image/img9.jpg'
+import J from '../../../../image/img10.jpg'
+
 class Fm extends Component {
     render(){
-        var arr = [0,1,2,3,4,5,6,7,8]
+        // var arr = [0,1,2,3,4,5,6,7,8]
+        var arr = [
+            {source: A, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: B, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: C, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: D, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: E, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: F, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: G, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: H, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: I, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456},
+            {source: J, name: "晓风残月", content: "这首歌是我听到的最好听的歌，哈哈哈", time:"2015年8月15日 15:23",goodNum:23456}
+        ]
         return (
             <div className="Fm_warper">
                 <div>
@@ -24,7 +47,7 @@ class Fm extends Component {
                         <div>
                             {
                                 arr.map(function(item){
-                                    return <Comment_list item={ item }/>
+                                    return <Comment_list source={item.source} name={item.name} content={item.content} time={item.time} goodNum={item.goodNum}/>
                                 })
                             }
                         </div>
