fb7c934ee7e71db729cca2fa7dac7c2b984605e6
my <my@qq.com>
2017-09-14 20:28:28 +0800

    适应小屏幕

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const {app, BrowserWindow} = require('electron')
 let win
 function createWindow(){
   win = new BrowserWindow({
-    width:1100, 
-    height:850,
+    width:900, 
+    height:750,
     frame:false,
-    minWidth:1000,
-    minHeight:750,
+    minWidth:800,
+    minHeight:600,
     transparent:false,
   })
   win.loadURL(`file://${__dirname}/index.html`)
