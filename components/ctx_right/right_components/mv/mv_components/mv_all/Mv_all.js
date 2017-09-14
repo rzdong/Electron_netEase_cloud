8022fdfd17c430582e8f0774827d39fa9fbb047a
my <my@qq.com>
2017-09-15 00:36:56 +0800

    添加mv页面

---
+++ components/ctx_right/right_components/mv/mv_components/mv_all/Mv_all.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { 
+    Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
+import './Mv_all.css';
+
+class Mv_all extends Component {
+    render(){
+        return (
+            <div>全部MV</div>
+        )
+    }
+}
+
+export default Mv_all;
---
+++ components/ctx_right/right_components/mv/mv_components/mv_one/Mv_one.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { 
+    Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
+import './Mv_one.css';
+
+class Mv_one extends Component {
+    render(){
+        return (
+            <div>MV精选</div>
+        )
+    }
+}
+
+export default Mv_one;
